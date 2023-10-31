b414772d718b2953fff3a677c8fc6bbb537052cb
Garrett Smith <garrettarhsmith@gmail.com>
2023-10-31 08:33:07 -0400

    fix import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <h1 className={s.logo}>Garrett Smith</h1>
-      <p className={s.subtitle}>Software Engineer</p>
+      <p className={s.subtitle}>Software Engineer - This portfolio is a work in progress</p>
     </>
   )
 }
