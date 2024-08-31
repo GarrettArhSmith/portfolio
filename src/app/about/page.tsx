5c70ff9628cc46edcea82f7e49a4d3dc5d64e92f
Garrett Smith <garrettarhsmith@gmail.com>
2024-08-31 18:52:38 -0400

    quick update of about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,30 +4,27 @@ import Image from "next/image";
 type Props = {};
 
 export default function page({}: Props) {
+  const pics = new Array(5).fill({});
+
   return (
     <main className="grid gap-12 place-items-center min-h-screen flex-col p-4 md:p-10 text-center">
-      <h2 className="max-w-[702px] text-4xl">
-        React Engineer with 3+ years of software development expertise,
-        dedicated to delivering high-quality, scalable, production code.
-        Proficient in a wide array of web technologies, and always hungry to
-        learn new programming languages and emerging technologies. Eager to join
-        a dynamic team with diverse talent.
-      </h2>
-      <div className="flex gap-8">
-        <Image
-          src="/picofme.jpg"
-          width={250}
-          height={250}
-          alt="Picture of Me"
-          className="rounded-3xl shadow-lg"
-        />
-        <Image
-          src="/picofme.jpg"
-          width={200}
-          height={100}
-          alt="Picture of Me"
-          className="rounded-3xl shadow-lg"
-        />
+      <div>
+        <h1 className="text-7xl font-bold">Garrett Smith</h1>
+        <h2 className="max-w-[702px] text-2xl">
+          Web engineer delivering high-quality, scalable code.
+        </h2>
+      </div>
+      <div className="flex gap-10 items-center">
+        {pics.map((pic, i) => (
+          <Image
+            key={i}
+            src={`/picofme${i + 1}.jpg`}
+            width={250}
+            height={200}
+            alt="Picture of Me"
+            className="rounded-3xl shadow-lg"
+          />
+        ))}
       </div>
     </main>
   );
