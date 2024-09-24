915f03bd75709fc426dd93f504343d03ca10042a
Garrett Smith <garrettarhsmith@gmail.com>
2024-09-24 18:36:29 -0400

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/app/components/Header/Header";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Analytics />
         <Header />
         <div className="rounded-3xl bg-slate-200 w-full max-w-full min-h-dvh bg-gradient-to-br from-rose-50 to-slate-200">
           {children}
